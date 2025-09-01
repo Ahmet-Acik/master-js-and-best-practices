4cab1921191d9908613cd21c61024b3b4007554f
drahmetacik <a.acik@icloud.com>
2025-09-01 08:27:35 +0100

    feat: add modules_bundling.js with examples of module exports and best practices for bundling

---
+++ 07_best_practices/modules_bundling.js
@@ -0,0 +1,13 @@
+// modules_bundling.js
+// Modules and bundling in JavaScript
+
+// utils.js
+export function square(x) {
+  return x * x;
+}
+
+// main.js
+// import { square } from './utils.js';
+// console.log(square(5));
+
+// Best practice: Use ES modules for modern projects. Use bundlers like Webpack or Vite for larger apps.
